50aaa517489d91e4d3b6dbc0d4e5931abdebe228
Pierre Hérissé <pierre.herisse@gmail.com>
2019-03-04 23:41:28 +0100

    2nd carousel in progress

--- adepem_v3.js
+++ adepem_v3.js
@@ -1,51 +1,83 @@
-
-//************ CAROUSEL LEFT FUNCTION **************
-const goLeft = (container,element)=>{
-  if ($(element).index($(container).find(element).filter(":visible:first"))+1===$(element).index($(container).find(element).filter(":visible:last"))) {
-    let firstBest = $(container).find(element).filter(":visible:first");
-    let lastBest = $(container).find(element).filter(":visible:last");;
+//************ 1ST CAROUSEL LEFT FUNCTION **************
+const goBestLeft = ()=>{
+  if ($(".best_product").index($(".carousel_bests").find(".best_product").filter(":visible:first"))+1===$(".best_product").index($(".carousel_bests").find(".best_product").filter(":visible:last"))) {
+    let firstBest = $(".carousel_bests").find(".best_product").filter(":visible:first");
+    let lastBest = $(".carousel_bests").find(".best_product").filter(":visible:last");;
     let prev;
-    if ($(element).index(firstBest)<=0) prev = $(element).length+2;
-    else prev = $(element).index(firstBest)+2;
+    if ($(".best_product").index(firstBest)<=0) prev = $(".best_product").length+2;
+    else prev = $(".best_product").index(firstBest)+2;
 
-    lastBest.hide();
-    $(firstBest).before($(`${element}:nth-child(${prev})`).show("fast"));
+
+    lastBest.hide('fast');
+    $(firstBest).before($(`${".best_product"}:nth-child(${prev})`).show("fast"));
   }
   return false;
 }
 
-//************ CAROUSEL RIGHT FUNCTION **************
-const goRight = (container,element)=>{
-  if ($(element).index($(container).find(element).filter(":visible:first"))+1===$(element).index($(container).find(element).filter(":visible:last"))) {
-    let firstBest = $(container).find(element).filter(":visible:first");
-    let lastBest = $(container).find(element).filter(":visible:last");;
+//************ 1ST CAROUSEL RIGHT FUNCTION **************
+const goBestRight = ()=>{
+  if ($(".best_product").index($(".carousel_bests").find(".best_product").filter(":visible:first"))+1===$(".best_product").index($(".carousel_bests").find(".best_product").filter(":visible:last"))) {
+    let firstBest = $(".carousel_bests").find(".best_product").filter(":visible:first");
+    let lastBest = $(".carousel_bests").find(".best_product").filter(":visible:last");;
 
     let next;
-    if ($(element).index(lastBest)+3>=$(element).length+2) {
+    if ($(".best_product").index(lastBest)+3>=$(".best_product").length+2) {
       next = 3;
-    } else next = $(element).index(lastBest)+4;
+    } else next = $(".best_product").index(lastBest)+4;
 
     firstBest.hide("fast");
-    $(lastBest).after($(`${element}:nth-child(${next})`).show("fast"));
+    $(lastBest).after($(`${".best_product"}:nth-child(${next})`).show("fast"));
   }
   return false;
+}
+
+//************ 2ND CAROUSEL LEFT FUNCTION **************
+const goBestModelLeft = ()=>{
+/*  if ($(".model_brand").index($(".best_models_brands").find(".model_brand").filter(":visible:first"))+1===$(".model_brand").index($(".best_models_brands").find(".best_product").filter(":visible:last"))) {*/
+    let firstModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:first");
+    let lastModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:last");;
+    let prev;
+    if ($(".model_brand").index(firstModelBest)<=0) prev = $(".model_brand").length+2;
+    else prev = $(".model_brand").index(firstModelBest)+2;
+
+    if ($(".model_brand").index(firstModelBest)===0) $(".left_arrow_models").hide("fast");
+    if ($(".model_brand").index(lastModelBest)===$(".model_brand").length-2) $(".right_arrow_models").hide("fast");
+      else ($(".right_arrow_models").show("fast"));
+
+    lastModelBest.hide('fast');
+    $(firstModelBest).before($(`${".model_brand"}:nth-child(${prev})`).show("fast"));
+/*  }
+  return false;*/
+}
+
+//************ 2ND CAROUSEL RIGHT FUNCTION **************
+const goBestModelRight = ()=>{
+/*  if ($(".model_brand").index($(".best_models_brands").find(".model_brand").filter(":visible:first"))+1===$(".model_brand").index($(".best_models_brands").find(".model_brand").filter(":visible:last"))) {*/
+    let firstModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:first");
+    console.log("first : "+$(".model_brand").index(firstModelBest));
+    let lastModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:last");
+    console.log("last : "+$(".model_brand").index(lastModelBest));
+
+    let next;
+    if ($(".model_brand").index(lastModelBest)+2>=$(".model_brand").length) {
+      next = 2;
+    } else next = $(".model_brand").index(lastModelBest)+6;
+    console.log("next : "+next);
+
+    firstModelBest.hide("fast");
+    $(lastModelBest).after($(`${".model_brand"}:nth-child(${next})`).show("fast"));
+
+    firstModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:first");
+    console.log("first (end): "+$(".model_brand").index(firstModelBest));
+    if ($(".model_brand").index(lastModelBest)===$(".model_brand").length-4) $(".right_arrow_models").hide("fast");
+    if ($(".model_brand").index(firstModelBest)>=1) $(".left_arrow_models").show("fast");
+  /*}
+  return false;*/
 }
 
 //************ FUNCTION CALLS **************
-$(".left_arrow").click( (event) => {
-  goLeft(".carousel_bests",".best_product");
-});
+$(".left_arrow").click(() => goBestLeft());
+$(".right_arrow").click(() => goBestRight());
 
-$(".right_arrow").click( (event) => {
-  goRight(".carousel_bests",".best_product");
-});
-
-$(".left_arrow_models").click( (event) => {
-  alert('left model');
-  goLeft(".best_models_brands",".model_brand");
-});
-
-$(".right_arrow_models").click( (event) => {
-  alert('right model');
-  goRight(".best_models_brands",".model_brand");
-});
+$(".left_arrow_models").click(() => goBestModelLeft());
+$(".right_arrow_models").click(() => goBestModelRight());
