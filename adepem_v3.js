dfbc1f75ed0c3dc8c1757bd57cb48729d61539e4
efrei <efrei@B15-04.local>
2019-03-04 12:21:03 +0100

    added first carousel

--- adepem_v3.js
+++ adepem_v3.js
@@ -1,22 +1,51 @@
-$(".best_product:nth-child(3)").hide();
-$(".best_product:nth-child(4)").hide();
-$(".best_product:nth-child(5)").hide();
-$(".best_product:nth-child(6)").hide();
 
-$(".left_arrow").click( (event) => {
-  let firstBest = $(".carousel_bests").find(".best_product").filter(":visible:first");
-    if (firstBest) console.log('first : '+$(".best_product").index(firstBest));
-  let lastBest = $(".carousel_bests").find(".best_product").filter(":visible:last");;
-    if (lastBest) console.log('last : '+$(".best_product").index(lastBest));
-  let prev;
+//************ CAROUSEL LEFT FUNCTION **************
+const goLeft = (container,element)=>{
+  if ($(element).index($(container).find(element).filter(":visible:first"))+1===$(element).index($(container).find(element).filter(":visible:last"))) {
+    let firstBest = $(container).find(element).filter(":visible:first");
+    let lastBest = $(container).find(element).filter(":visible:last");;
+    let prev;
+    if ($(element).index(firstBest)<=0) prev = $(element).length+2;
+    else prev = $(element).index(firstBest)+2;
 
-  if ($(".best_product").index(firstBest)<=0) prev = 6;
-  else prev = $(".best_product").index(firstBest)-1;  //gets the -1 element
-  console.log("prev : "+prev);
-
-  for (let i=$(".best_product").length;i>=2;i--) {
-    $(`.best_product:nth-child(${i})`).hide();
+    lastBest.hide();
+    $(firstBest).before($(`${element}:nth-child(${prev})`).show("fast"));
   }
-  lastBest.hide();
-  $(`.best_product:nth-child(${prev})`).show("fast");
+  return false;
+}
+
+//************ CAROUSEL RIGHT FUNCTION **************
+const goRight = (container,element)=>{
+  if ($(element).index($(container).find(element).filter(":visible:first"))+1===$(element).index($(container).find(element).filter(":visible:last"))) {
+    let firstBest = $(container).find(element).filter(":visible:first");
+    let lastBest = $(container).find(element).filter(":visible:last");;
+
+    let next;
+    if ($(element).index(lastBest)+3>=$(element).length+2) {
+      next = 3;
+    } else next = $(element).index(lastBest)+4;
+
+    firstBest.hide("fast");
+    $(lastBest).after($(`${element}:nth-child(${next})`).show("fast"));
+  }
+  return false;
+}
+
+//************ FUNCTION CALLS **************
+$(".left_arrow").click( (event) => {
+  goLeft(".carousel_bests",".best_product");
+});
+
+$(".right_arrow").click( (event) => {
+  goRight(".carousel_bests",".best_product");
+});
+
+$(".left_arrow_models").click( (event) => {
+  alert('left model');
+  goLeft(".best_models_brands",".model_brand");
+});
+
+$(".right_arrow_models").click( (event) => {
+  alert('right model');
+  goRight(".best_models_brands",".model_brand");
 });
