52bf861def94ecaddddb267eafb7f6587c89b4c8
Pierre Hérissé <pierre.herisse@gmail.com>
2019-03-05 19:54:11 +0100

    javascript : done; mobile responsive : 1/3

--- adepem_v3.js
+++ adepem_v3.js
@@ -9,7 +9,7 @@ const goBestLeft = ()=>{
 
 
     lastBest.hide('fast');
-    $(firstBest).before($(`${".best_product"}:nth-child(${prev})`).show("fast"));
+    $(firstBest).before($(`.best_product:nth-child(${prev})`).show("fast"));
   }
   return false;
 }
@@ -26,7 +26,7 @@ const goBestRight = ()=>{
     } else next = $(".best_product").index(lastBest)+4;
 
     firstBest.hide("fast");
-    $(lastBest).after($(`${".best_product"}:nth-child(${next})`).show("fast"));
+    $(lastBest).after($(`.best_product:nth-child(${next})`).show("fast"));
   }
   return false;
 }
@@ -36,37 +36,44 @@ const goBestModelLeft = ()=>{
     let firstModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:first");
     let lastModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:last");;
     let prev;
-    if ($(".model_brand").index(firstModelBest)<=0) prev = $(".model_brand").length+2;
-    else prev = $(".model_brand").index(firstModelBest)+2;
-
-    if ($(".model_brand").index(firstModelBest)===0) $(".left_arrow_models").hide("fast");
-    if ($(".model_brand").index(lastModelBest)===$(".model_brand").length-2) $(".right_arrow_models").hide("fast");
-      else ($(".right_arrow_models").show("fast"));
+    if ($(".model_brand").index(firstModelBest)<=1) {
+      prev = $(".model_brand").index(firstModelBest);
+      $(".left_arrow_models").hide("fast");
+      $(`.model_brand:nth-child(${$(".model_brand").index(lastModelBest)})`).css("border-right","0");
+    } else {
+      prev = $(".model_brand").index(firstModelBest);
+      $(`.model_brand:nth-child(${$(".model_brand").index(lastModelBest)})`).css("border-right","0");
+    }
 
     lastModelBest.hide('fast');
-    $(firstModelBest).before($(`${".model_brand"}:nth-child(${prev})`).show("fast"));
+    $(firstModelBest).before($(`.model_brand:nth-child(${prev})`).show("fast"));
 
-    firstModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:first");
-    if ($(".model_brand").index(firstModelBest)<1) $(".left_arrow_models").hide("fast");
+    if ($(".model_brand").index(lastModelBest)<$(".model_brand").length) $(".right_arrow_models").show("fast");
 }
 
 //************ 2ND CAROUSEL RIGHT FUNCTION **************
 const goBestModelRight = ()=>{
     let firstModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:first");
     let lastModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:last");
-
     let next;
-    if ($(".model_brand").index(lastModelBest)+3>=$(".model_brand").length) {
+    if ($(".model_brand").index(lastModelBest)+2>=$(".model_brand").length) { //If we reach the last element
       next = $(".model_brand").length;
       $(".right_arrow_models").hide("fast");
-    } else next = $(".model_brand").index(lastModelBest)+5;
+      $(`.model_brand:nth-child(${next})`).css("border-right","0");
+    } else next = $(".model_brand").index(lastModelBest)+2;
 
     firstModelBest.hide("fast");
-    $(lastModelBest).after($(`${".model_brand"}:nth-child(${next})`).show("fast"));
+    $(lastModelBest).after($(`.model_brand:nth-child(${next})`).show("fast"));
 
+    // Show left arrow if firstmodel is >= 2nd element of the list
     firstModelBest = $(".best_models_brands").find(".model_brand").filter(":visible:first");
-
     if ($(".model_brand").index(firstModelBest)>=0) $(".left_arrow_models").show("fast");
+
+    // Column borders managing
+    if (next<=$(".model_brand").length+1) {
+      $(`.model_brand:nth-child(${$(".model_brand").index(lastModelBest)+1})`).css("border-right","1px solid rgba(128, 128, 128, .2)");
+      $(`.model_brand:nth-child(${next+1})`).css("border-right","0");
+    } else $(`.model_brand:nth-child(6)`).css("border-right","0");
 }
 
 
@@ -76,3 +83,16 @@ $(".right_arrow").click(() => goBestRight());
 
 $(".left_arrow_models").click(() => goBestModelLeft());
 $(".right_arrow_models").click(() => goBestModelRight());
+
+
+//************ SHOW MORE PIECES ***************
+$('.show_more_pieces').click(()=>{
+  $('.show_more_pieces').hide("fast");
+  $('.pieces_list_line').show('slow');
+});
+
+//************ SHOW MORE BRANDS ***************
+$('.show_more_brands').click(()=>{
+  $('.show_more_brands').hide("fast");
+  $('.brands_list_line').show('slow');
+});
